e85d3380df222e1c8f32e2a5c2564056311008fa
GOyan93 <goyandle@gmail.com>
2021-07-12 17:31:19 -0400

    Write program layout

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -13,3 +13,51 @@ bringing the highest value to the end of the list after each iteration.
 The tests for this algorithm will use end cases involving the '0' integer, 
 negative integers floating point numericals, and other inputs.
 """
+
+
+##########################
+## BUBBLE SORT FUNCTION ##
+##########################
+
+def bubble_sort(arr):
+    pass
+
+
+####################
+## TEST FUNCTIONS ##
+####################
+
+# Determine sorting of a list of integers from 1-9.
+def int_test(func):
+    pass
+
+# Determine sorting of a list with integer 0.
+def zero_test(func):
+    pass
+
+# Determine sorting of a list with negative integer.
+def neg_test(func):
+    pass
+
+# Determine sorting of a list with floating point numbers.
+def flt_test(func):
+    pass
+
+# Determine sorting of a list with 0.0 floating point.
+def zero_flt_test(func):
+    pass
+
+# Determine sorting of a list with negative floating point numbers.
+def neg_flt_test(func):
+    pass
+
+# Determine sorting of a list with non-list values.
+def non_list_test(func):
+    pass
+
+
+
+
+
+    
+
