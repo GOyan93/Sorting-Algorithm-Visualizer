29985ca442e51b4094ff58b78396b55b9f014f5d
GOyan93 <goyandle@gmail.com>
2021-07-12 17:03:14 -0400

    Introduction document for Bubble Sort

---
+++ Bubble_Sort.py
@@ -0,0 +1,15 @@
+/#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Title: Bubble Sort Algorithm
+Created by: Greg Yandle
+Date (DD/MM/YYYY): 09/07/2021 
+"""
+
+"""
+The bubble sort algorithm will be used within a sorting visualizer.
+This algorithm will take a list of numbers and will interate over the list 
+bringing the highest value to the end of the list after each iteration.
+The tests for this algorithm will use end cases involving the '0' integer, 
+negative integers floating point numericals, and other inputs.
+"""
