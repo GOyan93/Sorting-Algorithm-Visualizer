5982800e99aa26833f5c66d79ba3731369dfd8f8
GOyan93 <goyandle@gmail.com>
2021-07-13 16:42:23 -0400

    Migrate test functions to Unit_Tests.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -29,43 +29,7 @@ def bubble_sort(arr):
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
     return arr
-    
-####################
-## TEST FUNCTIONS ##
-####################
-
-# Determine sorting of a list of integers from 1-9.
-def int_test(func):
-    pass
-
-# Determine sorting of a list with integer 0.
-def zero_test(func):
-    pass
-
-# Determine sorting of a list with negative integer.
-def neg_test(func):
-    pass
-
-# Determine sorting of a list with floating point numbers.
-def flt_test(func):
-    pass
-
-# Determine sorting of a list with 0.0 floating point.
-def zero_flt_test(func):
-    pass
-
-# Determine sorting of a list with negative floating point numbers.
-def neg_flt_test(func):
-    pass
-
-# Determine sorting of a list with non-list values.
-def non_list_test(func):
-    pass
-
-# Determine sorting of a single digit list.
-def single_test(func):
-    pass
-
+ 
 
 
 
