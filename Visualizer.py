df3fd85496d3e15bb3eb4fbfd072663bc2fa12ab
GOyan93 <goyandle@gmail.com>
2021-07-27 16:48:32 -0400

    Visualizer GUI

---
+++ Visualizer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 16:44:30 2021
+
+@author: greg
+"""
+
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Sorting Algorithm Visualizer")
